6bfc236969b6e3a5d1547719ed75c5491e700a32
Vinicius Oliveira <eovinicius10@gmail.com>
2024-04-29 10:02:44 -0300

    update medico dto

--- Application/Dtos/input/CadastrarMedicoInput.cs
+++ Application/Dtos/input/CadastrarMedicoInput.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace SistemaHospitalar.Application.Dtos.input;
-public record CadastrarMedicoInput(string Nome, string Crm, [MinLength(6)] string Senha, string Especialidade);
+public record CadastrarMedicoInput(string Nome, string Crm, string Senha, string Especialidade);
 
