ba8cd306634d4b2309464dc473801920fe07e305
Vinicius Oliveira <eovinicius10@gmail.com>
2024-04-30 11:08:12 -0300

    Removed appointment repository functions

--- Application/Repositories/IConsultaRepository.cs
+++ Application/Repositories/IConsultaRepository.cs
@@ -1,12 +1,9 @@
 using SistemaHospitalar.Application.Dtos.output;
 using SistemaHospitalar.Domain.Entities;
-using SistemaHospitalar.Domain.Enums;
 
 namespace SistemaHospitalar.Application.Repositories;
 public interface IConsultaRepository : IRepository<Consulta>
 {
-    Task<List<Consulta>> GetConsultasByPaciente(Guid id, EStatusAtendimento? status = null);
-    Task<List<Consulta>> GetConsultasByMedico(Guid medicoId, EStatusAtendimento? status = null, DateTime? dataInicial = null, DateTime? dataFinal = null);
     Task<bool> ExistsConsultaMarcada(Guid medicoId, DateTime dataHora);
     Task<List<ListConsultaOutput>> GetAll(Pagination pagination);
 }
--- Infrastructure/Database/EntityFramework/Repositories/ConsultaRepository.cs
+++ Infrastructure/Database/EntityFramework/Repositories/ConsultaRepository.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using SistemaHospitalar.Domain.Entities;
-using SistemaHospitalar.Domain.Enums;
 using SistemaHospitalar.Application.Repositories;
 using SistemaHospitalar.Application.Dtos.output;
 
@@ -38,36 +37,6 @@ public class ConsultaRepository : IConsultaRepository
     {
         return await _context.Consultas.Include(x => x.Laudo).FirstOrDefaultAsync(c => c.Id == id);
     }
-
-    public async Task<List<Consulta>> GetConsultasByMedico(Guid medicoId, EStatusAtendimento? status = null, DateTime? dataInicial = null, DateTime? dataFinal = null)
-    {
-        IQueryable<Consulta> query = _context.Consultas
-            .Where(c => c.MedicoId == medicoId);
-
-        if (status != null)
-            query = query.Where(c => c.Status == status);
-
-        if (dataInicial != null)
-            query = query.Where(c => c.DataHora >= dataInicial);
-
-        if (dataFinal != null)
-            query = query.Where(c => c.DataHora <= dataFinal);
-
-        return await query.ToListAsync();
-    }
-
-    public async Task<List<Consulta>> GetConsultasByPaciente(Guid id, EStatusAtendimento? status = null)
-    {
-        IQueryable<Consulta> query = _context.Consultas
-            .Where(c => c.PacienteId == id);
-
-        if (status != null)
-            query = query.Where(c => c.Status == status);
-
-        return await query.ToListAsync();
-    }
-
-
     public Task Update(Consulta consulta)
     {
         _context.Consultas.Update(consulta);
